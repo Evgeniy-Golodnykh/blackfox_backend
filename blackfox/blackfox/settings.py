68de1df63dd296e88ca8291af296c951a32e80ad
Evgeniy Golodnykh <evgenferrum@gmail.com>
2023-06-16 16:26:09 +0300

    add git workflow

--- blackfox/blackfox/settings.py
+++ blackfox/blackfox/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y(v_!hanxm+18(y-fu$5$_!7a#iv50tv(^7xoae(7ci3_%mx^h'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'NOT_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', 'NO_HOSTS')]
 
 
 # Application definition
