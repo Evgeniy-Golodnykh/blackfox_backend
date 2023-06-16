a57c69dc93ac3181c88124747102b206b39c2c62
Evgeniy Golodnykh <evgenferrum@gmail.com>
2023-06-16 16:40:17 +0300

    add allowed hosts

--- blackfox/blackfox/settings.py
+++ blackfox/blackfox/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'NOT_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', 'NO_HOSTS')]
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', 'NO_HOSTS'), '127.0.0.1']
 
 
 # Application definition
