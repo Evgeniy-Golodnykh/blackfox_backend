81f12b7faf52b8b861d93ad9315016576b5d6835
K1N88 <m-end-eleev@yandex.ru>
2023-07-16 16:37:52 +0300

    add diet parser url views

--- blackfox/training/services.py
+++ blackfox/training/services.py
@@ -5,8 +5,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-from training.models import Diet
-
 
 TEST_URL = 'https://health-diet.ru/diary/share/7758976496ec140b28a'
 SLEEP = 3
@@ -24,6 +22,21 @@ FIELDS = {
     40: 'Железо, Fe (мг)',
     83: 'Холестерин (мг)',
 }
+DIET = [
+    'calories', 'calories_rdr', 'calories_perc',
+    'protein', 'protein_rdr', 'protein_perc',
+    'fat', 'fat_rdr', 'fat_perc',
+    'carb', 'carb_rdr', 'carb_perc',
+    'fiber', 'fiber_rdr', 'fiber_perc',
+    'water', 'water_rdr', 'water_perc',
+    'vitamin_d', 'vitamin_d_rdr', 'vitamin_d_perc',
+    'potassium', 'potassium_rdr', 'potassium_perc',
+    'calcium', 'calcium_rdr', 'calcium_perc',
+    'natrium', 'natrium_rdr', 'natrium_perc',
+    'ferrum', 'ferrum_rdr', 'ferrum_perc',
+    'cholesterin', 'cholesterin_rdr', 'cholesterin_perc',
+]
+CENTURY = '20'
 
 
 def get_data(url):
@@ -35,12 +48,21 @@ def get_data(url):
     # Возможные варианты для поиска:
     # ID, XPATH, LINK_TEXT, PARTIAL_LINK_TEXT,
     # NAME, TAG_NAME, CLASS_NAME, CSS_SELECTOR
-    overflow_button = driver.find_element(By.CSS_SELECTOR, '#js_app_root > div.uk-height-1-1 > div.mzr-grid-1-column > div > div > div > div:nth-child(5) > div > div.uk-flex-item-1.uk-margin-large-left > div > div.mzr-block-header-light.uk-flex.uk-flex-space-between.uk-flex-middle.mzr-font--body16sb > div.uk-flex.uk-flex-middle.uk-flex-item-none.mzr-no-print > svg:nth-child(1)')
+    overflow_button = driver.find_element(
+        By.CSS_SELECTOR,
+        '#js_app_root > div.uk-height-1-1 > div.mzr-grid-1-column > div > div > div > div:nth-child(5) > div > div.uk-flex-item-1.uk-margin-large-left > div > div.mzr-block-header-light.uk-flex.uk-flex-space-between.uk-flex-middle.mzr-font--body16sb > div.uk-flex.uk-flex-middle.uk-flex-item-none.mzr-no-print > svg:nth-child(1)'
+    )
     overflow_button.click()
     sleep(SLEEP)
 
-    date = driver.find_element(By.CSS_SELECTOR, '#js_app_root > div.uk-height-1-1 > div.mzr-grid-1-column > div > div > div > div.mzr-block-header-light.uk-flex.uk-flex-space-between.uk-flex-middle.mzr-font--body18sb > div:nth-child(1)').text.split(' ')[-1]
-    table = driver.find_element(By.CSS_SELECTOR, '#js_app_root > div.uk-height-1-1 > div.mzr-grid-1-column > div > div > div > div:nth-child(5) > div > div.uk-flex-item-1.uk-margin-large-left > div > div.uk-overflow-container > table > tbody')
+    date = driver.find_element(
+        By.CSS_SELECTOR,
+        '#js_app_root > div.uk-height-1-1 > div.mzr-grid-1-column > div > div > div > div.mzr-block-header-light.uk-flex.uk-flex-space-between.uk-flex-middle.mzr-font--body18sb > div:nth-child(1)'
+    ).text.split(' ')[-1]
+    table = driver.find_element(
+        By.CSS_SELECTOR,
+        '#js_app_root > div.uk-height-1-1 > div.mzr-grid-1-column > div > div > div > div:nth-child(5) > div > div.uk-flex-item-1.uk-margin-large-left > div > div.uk-overflow-container > table > tbody'
+    )
     rows = table.find_elements(By.TAG_NAME, 'tr')
     results = []
 
@@ -53,32 +75,20 @@ def get_data(url):
     results.append(date)
 
     driver.quit()  # Закрытие веб-драйвера.
-    return results
+    return edit_data(results, date)
 
 
-def edit_data(data):
-    columns = Diet.__dir__[3:39]
-    print(columns)
+def edit_data(data, diet_date):
     results = {}
-    results['diet_date'] = data[-1]
+    dd, mm, yy = diet_date.split('.')
+    results['diet_date'] = CENTURY + yy + '-' + mm + '-' + dd
     temp = []
     for n in FIELDS.keys():
         vavue, rdr, percentage = data[n]
         temp.append(vavue)
-        temp.append(rdr)
-        temp.append(percentage)
-    for i in range(len(columns)):
-        results[i] = float(temp[i])
-
-    print(results)
+        temp.append(rdr.split('-')[-1].split(' ')[-1])
+        temp.append(percentage.replace('%', ''))
+    for i in range(len(DIET)):
+        results[DIET[i]] = float(temp[i])
 
     return results
-
-
-if __name__ == '__main__':
-    columns = Diet.__dir__[3:39]
-    print(columns)
-    '''
-    data = get_data(TEST_URL)
-    edit_data(data)
-    '''
