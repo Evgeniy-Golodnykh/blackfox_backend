870c8d56d267241eddb5a5f87a9d6e7dc0b69f56
Evgeniy Golodnykh <evgenferrum@gmail.com>
2024-06-18 21:56:31 +0300

    project serializer

--- blackfox/api/serializers.py
+++ blackfox/api/serializers.py
@@ -29,8 +29,14 @@ class ProjectSerializer(serializers.ModelSerializer):
     coach = UserSerializer(read_only=True)
     start_date = serializers.DateField()
     deadline = serializers.DateField()
+    target_calories = serializers.IntegerField(min_value=0, max_value=10_000)
+    target_carbohydrate = serializers.FloatField(min_value=0, max_value=1_000)
+    target_fat = serializers.FloatField(min_value=0, max_value=1_000)
+    target_fiber = serializers.FloatField(min_value=0, max_value=1_000)
+    target_protein = serializers.FloatField(min_value=0, max_value=1_000)
+    target_sugar = serializers.FloatField(min_value=0, max_value=1_000)
     target_weight = serializers.FloatField(min_value=30, max_value=250)
 
     class Meta:
         model = Project
-        fields = ['user', 'coach', 'start_date', 'deadline', 'target_weight']
+        exclude = ['id', 'is_closed']
