dfb6137bf90860a148de04c06bb4b1db3b9b3ba5
Evgeniy Golodnykh <evgenferrum@gmail.com>
2024-11-28 22:50:33 +0300

    add crontab to autoupdate fatsecret data, update fatsecret views and tools

---
+++ blackfox/api/cron.py
@@ -0,0 +1,40 @@
+import logging
+import time
+
+from django.contrib.auth import get_user_model
+from django.db.models import Q
+
+from fatsecret.tools import get_fooddiary_objects
+from training.models import FoodDiary, Project
+
+DATETIME_FORMAT = '%d.%m.%Y %H:%M:%S'
+LOGFORMAT = '%(asctime)s [%(levelname)s] %(filename)s/%(funcName)s %(message)s'
+
+User = get_user_model()
+error_message = 'Updating data for user "{user}" failed with error "{err}"'
+successful_message = 'Autoupdate data for user "{user}" completed successfully'
+
+logging.basicConfig(
+    datefmt=DATETIME_FORMAT,
+    format=LOGFORMAT,
+    level=logging.INFO,
+)
+
+
+def fooddiary_autoupdate():
+    """Autoupdate users Fatsecret data."""
+
+    users = User.objects.filter(
+        Q(fatsecret_token__isnull=False) & Q(fatsecret_secret__isnull=False)
+    )
+    for user in users:
+        if not Project.objects.filter(user=user).exists():
+            continue
+        try:
+            objs = get_fooddiary_objects(user)
+        except Exception as err:
+            logging.error(error_message.format(user=user.username, err=err))
+            continue
+        FoodDiary.objects.bulk_create(objs=objs)
+        logging.info(successful_message.format(user=user.username))
+        time.sleep(5)
