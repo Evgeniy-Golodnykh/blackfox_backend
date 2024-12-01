10f5a048c45e3f48b136c3176564b0a3faae94b4
Evgeniy Golodnykh <evgenferrum@gmail.com>
2024-12-02 00:32:47 +0300

    update crontab settings and project serializer

--- blackfox/api/serializers.py
+++ blackfox/api/serializers.py
@@ -10,7 +10,8 @@ User = get_user_model()
 
 current_date = dt.date.today()
 error_coach_message = 'The coach does not have the appropriate role'
-error_date_message = 'The date cannot be greater than the current one'
+error_big_date_message = 'The date cannot be greater than the current one'
+error_small_date_message = 'The date cannot be less than 15 days ago'
 error_user_message = 'The user is admin or coach, please choose another one'
 diary_entry_exists_message = (
     'A diary entry for the current user and date already exists'
@@ -37,7 +38,7 @@ class CreateUpdateBodyStatsDiarySerializer(serializers.ModelSerializer):
 
     def validate_date(self, input_date):
         if input_date > current_date:
-            raise serializers.ValidationError(error_date_message)
+            raise serializers.ValidationError(error_big_date_message)
         return input_date
 
     def create(self, validated_data):
@@ -89,7 +90,9 @@ class CreateUpdateProjectSerializer(ProjectSerializer):
 
     def validate_start_date(self, input_date):
         if input_date > current_date:
-            raise serializers.ValidationError(error_date_message)
+            raise serializers.ValidationError(error_big_date_message)
+        if input_date < current_date - dt.timedelta(15):
+            raise serializers.ValidationError(error_small_date_message)
         return input_date
 
     def validate_coach(self, user):
