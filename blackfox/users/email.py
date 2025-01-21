c34dc7cc418332b8d6b6acaebab74f7fd0bbcf3c
Evgeniy Golodnykh <evgenferrum@gmail.com>
2025-01-21 21:53:47 +0300

    update email context processor and message templates

--- blackfox/users/email.py
+++ blackfox/users/email.py
@@ -1,9 +1,17 @@
-import datetime
+from datetime import datetime as dt
 
 from django.contrib.auth import get_user_model
 from djoser import email
 
 User = get_user_model()
+intro_message = (
+    'Cпасибо за регистрацию',
+    'Вы изменили адрес электронной почты'
+)
+close_message = (
+    'Если вы не регистрировались',
+    'Если вы не изменяли адрес электронной почты'
+)
 
 
 class ActivationEmail(email.ActivationEmail):
@@ -19,10 +27,14 @@ class PasswordResetEmail(email.PasswordResetEmail):
 
 
 def email_context_processor(request):
-    if not hasattr(request, 'data'):
+    if not hasattr(request, 'data') or not request.data.get('email'):
         return {}
-    email = request.data.get('email')
+    current_date = dt.now()
+    user = User.objects.filter(email=request.data.get('email')).first()
+    email_changed = current_date.date() > user.date_joined.date()
     return {
-        'first_name': User.objects.filter(email=email).first().first_name,
-        'year': datetime.datetime.now().year
-    } if email else {}
+        'user_firstname': user.first_name,
+        'intro_message': intro_message[email_changed],
+        'close_message': close_message[email_changed],
+        'year': current_date.year,
+    }
