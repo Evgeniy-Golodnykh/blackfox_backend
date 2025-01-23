e194533fc3986b2ce8fcd08e1d004564827fed5f
Evgeniy Golodnykh <evgenferrum@gmail.com>
2025-01-24 00:12:11 +0300

    add weekly foods view, update fatsecret tools and url

--- blackfox/fatsecret/urls.py
+++ blackfox/fatsecret/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 from fatsecret.views import (
     AccessTokenView, FoodDiaryDailyView, FoodDiaryMonthlyView,
-    RequestTokenView, WeightDiaryView,
+    RequestTokenView, WeeklyFoodsView, WeightDiaryView,
 )
 
 urlpatterns = [
@@ -12,5 +12,6 @@ urlpatterns = [
     path('access/', AccessTokenView.as_view(), name='get_access_token'),
     path('weights/', WeightDiaryView.as_view(), name='weights'),
     path('foods_daily/', FoodDiaryDailyView.as_view(), name='foods_daily'),
+    path('foods_weekly/', WeeklyFoodsView.as_view(), name='foods_weekly'),
     path('foods_mothly/', FoodDiaryMonthlyView.as_view(), name='foods_monthly')
 ]
