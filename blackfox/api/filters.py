3d89b9115aff9e2734803202ca2a613b4d7426ce
Evgeniy Golodnykh <113941793+Evgeniy-Golodnykh@users.noreply.github.com>
2026-08-03 10:12:42 +0300

    Update filters.py

--- blackfox/api/filters.py
+++ blackfox/api/filters.py
@@ -2,19 +2,21 @@ from django_filters import rest_framework as filters
 
 
 class UniversalUserFilter(filters.FilterSet):
-    user = filters.CharFilter(method="filter_by_user")
+    user = filters.CharFilter(method='filter_by_user')
 
     def filter_by_user(self, queryset, name, value):
-        if value and hasattr(queryset.model, "user"):
+        if value and queryset.model.__name__ == 'User':
+            return queryset.filter(username=value)
+        if value and hasattr(queryset.model, 'user'):
             return queryset.filter(user__username=value)
         return queryset
 
 
 class UniversalCoachFilter(filters.FilterSet):
-    coach = filters.CharFilter(method="filter_by_coach")
+    coach = filters.CharFilter(method='filter_by_coach')
 
     def filter_by_coach(self, queryset, name, value):
-        if value and hasattr(queryset.model, "coach"):
+        if value and hasattr(queryset.model, 'coach'):
             return queryset.filter(coach__username=value)
         return queryset
 
