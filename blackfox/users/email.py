290026317732cf11d2e128132781074be70b12ad
Evgeniy Golodnykh <113941793+Evgeniy-Golodnykh@users.noreply.github.com>
2026-08-06 10:15:56 +0300

    Update email.py

--- blackfox/users/email.py
+++ blackfox/users/email.py
@@ -1,18 +1,16 @@
 from datetime import datetime as dt
 
-from django.contrib.auth import get_user_model
-from django.shortcuts import get_object_or_404
 from djoser import email
 
-User = get_user_model()
-intro_message = (
+intro_activation_message = (
     'Cпасибо за регистрацию',
     'Вы изменили адрес электронной почты'
 )
-close_message = (
+close_activation_message = (
     'Если вы не регистрировались',
     'Если вы не изменяли адрес электронной почты'
 )
+email_url = '/api/users/me'
 
 
 class ActivationEmail(email.ActivationEmail):
@@ -20,24 +18,18 @@ class ActivationEmail(email.ActivationEmail):
 
     template_name = 'email/activation.html'
 
+    def get_context_data(self):
+        context = super().get_context_data()
+        user = self.context.get('user')
+        email_changed = email_url in self.request.path
+        context['user_firstname'] = user.first_name
+        context['intro_message'] = intro_activation_message[email_changed]
+        context['close_message'] = close_activation_message[email_changed]
+        context['year'] = dt.now().year
+        return context
+
 
 class PasswordResetEmail(email.PasswordResetEmail):
     '''Override reset password email with template.'''
 
     template_name = 'email/password_reset.html'
-
-
-def email_context_processor(request):
-    '''Context processor to send email messages.'''
-
-    if hasattr(request, 'data') and 'email' in request.data:
-        current_date = dt.now()
-        user = get_object_or_404(User, email=request.data.get('email'))
-        email_changed = current_date.date() > user.date_joined.date()
-        return {
-            'user_firstname': user.first_name,
-            'intro_message': intro_message[email_changed],
-            'close_message': close_message[email_changed],
-            'year': current_date.year,
-        }
-    return dict()
